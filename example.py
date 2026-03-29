1b39a614b5c71c6a356ea61116a4b0b366642098
Rob Faludi <rob@faludi.com>
2026-03-29 17:06:10 -0400

    improvements to lookups and better timezone options.

--- example.py
+++ example.py
@@ -5,16 +5,27 @@ import time
 # The nature api is a library for connecting to realtime weather and natural events data, using open-meteo and other sources.
 # see the "example responses" directory for parameter options and example responses.
 
+# TODO:
+# different api for faster timezone setting (ipgeolocation.io?)
+# multiple parameters for astro get
+
+
+
 version = "0.1.1"
-DEBUG_MODE = False
+DEBUG_MODE = True
 
 print(f"Starting Nature API client v{version} ...")
+print(f"UsingNature API library version: {nature_api.__version__}")
 
 ssid = secrets.WIFI_SSID
 password = secrets.WIFI_PASSWORD
 
+
 # # Initialize the Nature API client
 client = nature_api.Client(ssid, password, default_refresh=300,status_led_pin="LED", debug_mode=DEBUG_MODE)
+
+# Set the API key for ipgeolocation.io (required for astronomy data)
+client.set_api_key("ipgeolocation", secrets.IPGEOLOCATION_API_KEY)
 
 # Connect to Wi-Fi
 client.connect_wifi()
@@ -40,20 +51,29 @@ print(f"Location set to: {client.get_address()}")
 # Set the timezone based on the location (using timeapi.io API)
 try:
     client.set_timezone_from_location()
-    print(f"Timezone set with UTC offset: {client.utc_offset} seconds")
+    print(f"Timezone set with UTC offset: {client.utc_offset/60/60} hours")
 except Exception as e:
     print(f"Error setting timezone: {e}")
 
-# Set the API key for ipgeolocation.io (required for astronomy data)
-client.set_api_key("ipgeolocation", secrets.IPGEOLOCATION_API_KEY)
-
 # Fetch some natural phenomena data at the specified location
 try:
-    cloud_cover = client.get_forecast("current", "cloud_cover")
-    print(f"Cloud cover at {client.get_address()} is: {cloud_cover}%")
+    results = client.get_forecast("current", "temperature_2m,cloud_cover,wind_speed_10m",)
+    print(f"Current temperature at {client.get_address()} is: {results['temperature_2m']} °C")
 
-    moon_illumination = client.get_astronomy("astronomy", "moon_illumination_percentage")
-    print(f"Moon illumination at {client.get_address()} is: {moon_illumination}%")
+    results = client.get_forecast("current", ["temperature_2m", "cloud_cover", "wind_speed_10m"])
+    temp = results["temperature_2m"]
+    cloud_cover = results["cloud_cover"]
+    speed = results["wind_speed_10m"]
+    print(f"Current weather at {client.get_address()}: {temp}°C, {cloud_cover}% cloud cover, {speed} km/h wind speed")
+
+    results = client.get_forecast("current", ["wind_speed_10m"])
+    print(f"Current wind speed at {client.get_address()}: {results['wind_speed_10m']} km/h")
+
+    results = client.get_forecast("current", "cloud_cover")
+    print(f"Cloud cover at {client.get_address()} is: {results['cloud_cover']}%")
+
+    # moon_illumination = client.get_astronomy("astronomy", "moon_illumination_percentage")
+    # print(f"Moon illumination at {client.get_address()} is: {moon_illumination}%")
 
     # wind_speed = client.get_forecast("current", "wind_speed_10m")
     # print(f"Wind speed at {client.get_address()} is: {wind_speed} km/h")
@@ -74,6 +94,7 @@ try:
     # print(f"Location set to: {address_hot}")
     # temps = client.get_forecast("current", "temperature_2m")
     # print(f"Current temperatures at {address_hot} is: {temps} °C")
+
 except Exception as e:
     print(f"Error fetching weather data: {e}")
 
--- nature_api.py
+++ nature_api.py
@@ -7,48 +7,25 @@ from Url_encode import url_encode
 import machine
 import ntptime
 
+__version__ = "0.1.3"
+
 class Client:
     def __init__(self, ssid, password, default_refresh=300, status_led_pin=None, debug_mode=False):
         self.ssid = ssid
         self.password = password
         self.default_refresh = default_refresh
         self.status_led_pin = status_led_pin
+        self.ipgeolocation_api_key = None
         self.wifi_connected = False
         self.address = None
         self.location = None
         self.utc_offset = 0
+        self.headers = {"User-Agent": "rp2"}  # Add a custom user agent
         self.debug_mode = debug_mode
 
         if self.status_led_pin is not None:
             self.led = machine.Pin(self.status_led_pin, machine.Pin.OUT)
             self.led.off()
-
-    # def connect_wifi(self, max_retries=5):
-
-    #     if self.status_led_pin is not None:
-    #         self.led.on()  # Turn on LED while connecting
-
-    #     wlan = network.WLAN(network.STA_IF)
-    #     wlan.active(True)
-    #     wlan.connect(self.ssid, self.password)
-
-    #     while not wlan.isconnected():
-    #         time.sleep(1)
-
-    #     self.wifi_connected = True
-
-    #     if self.status_led_pin is not None:
-    #         self.led.off()  # Turn off LED after connecting
-
-    # def connect_wifi(self, max_retries=10):
-    #         connection = False
-    #         connection_timeout = max_retries
-    #         while not connection:
-    #             connection = connect_to_wifi()
-    #             connection_timeout -= 1
-    #             if connection_timeout == 0:
-    #                 print('Could not connect to Wi-Fi, exiting')
-    #                 machine.reset()
                 
     def connect_wifi(self, attempts_per_cycle=10, max_attempts=10):
         while max_attempts > 0:
@@ -92,10 +69,10 @@ class Client:
             raise ValueError("Location is not set.")
         
         try:
-            headers = {
-                "User-Agent": "rp2"  # Add a custom user agent
-            }
-            response = requests.get(f"https://timeapi.io/api/v1/time/current/coordinate?latitude={self.location['latitude']}&longitude={self.location['longitude']}")
+            if self.ipgeolocation_api_key: 
+                response = requests.get(f"https://api.ipgeolocation.io/v3/timezone?apiKey={self.ipgeolocation_api_key}&lat={self.location['latitude']}&long={self.location['longitude']}", headers=self.headers, timeout=10)
+            else:        
+                response = requests.get(f"https://timeapi.io/api/v1/time/current/coordinate?latitude={self.location['latitude']}&longitude={self.location['longitude']}", headers=self.headers, timeout=10)
             if self.debug_mode:
                 print(response.content)
                 print('Response code: ', response.status_code)
@@ -103,6 +80,9 @@ class Client:
                 
             if 'utc_offset_seconds' in timezone_data:
                 self.utc_offset = timezone_data['utc_offset_seconds']
+            elif 'time_zone' in timezone_data and 'offset_with_dst' in timezone_data['time_zone']:
+                offset_str = timezone_data['time_zone']['offset_with_dst']
+                self.utc_offset = int(offset_str) * 60 * 60
             else:
                 raise ValueError("UTC offset not found in timezone data.")
         except Exception as e:
@@ -119,7 +99,7 @@ class Client:
             headers = {
                 "User-Agent": "rp2"  # Add a custom user agent
             }
-            response = requests.get(f"https://nominatim.openstreetmap.org/search?q={encoded_address}&format=json&limit=1", headers=headers, timeout=10)
+            response = requests.get(f"https://nominatim.openstreetmap.org/search?q={encoded_address}&format=json&limit=1", headers=self.headers, timeout=10)
             if self.debug_mode:
                 print(response.content)
             response_code = response.status_code
@@ -147,17 +127,23 @@ class Client:
         if not self.address:
             return None
         return self.address
-
-    def get_forecast(self, category ,parameter, forecast_days=1):
+    
+    def get_forecast(self, category, parameters, forecast_days=1):
         if not self.wifi_connected:
             raise ConnectionError("Wi-Fi is not connected.")
         
         if not self.location:
             raise ValueError("Location is not set.")
-
-        weather_url = f"https://api.open-meteo.com/v1/forecast?latitude={self.location['latitude']}&longitude={self.location['longitude']}&{category}={parameter}&forecast_days={forecast_days}"
         
-        response = requests.get(weather_url)
+        if type(parameters) is str:
+            parameters = parameters.split(",")  # Convert comma-separated string to list
+
+        params_string = ",".join(parameters)
+        if self.debug_mode:
+            print(f"Requesting forecast for parameters: {params_string}")  # Debugging line to check the requested parameters
+        weather_url = f"https://api.open-meteo.com/v1/forecast?latitude={self.location['latitude']}&longitude={self.location['longitude']}&{category}={params_string}&forecast_days={forecast_days}"
+        
+        response = requests.get(weather_url, headers=self.headers, timeout=10)
         data = response.json()
         if self.debug_mode:
             print(f"Weather data: {data}")  # Debugging line to check the weather data
@@ -165,14 +151,39 @@ class Client:
         if self.debug_mode:
             print('Response code: ', response_code)
 
-        if category in data and parameter in data[category]:
-            return data[category][parameter]
-        else:
-            raise ValueError(f"{parameter} is not available in the {category} weather data.")
+        results = {}
+        for parameter in parameters:
+            if category in data and parameter in data[category]:
+                results[parameter] = data[category][parameter]
+            else:
+                results[parameter] = None  # or you could choose to raise an error or skip it
+        return results
+
+    # def get_forecast(self, category ,parameter, forecast_days=1):
+    #     if not self.wifi_connected:
+    #         raise ConnectionError("Wi-Fi is not connected.")
+        
+    #     if not self.location:
+    #         raise ValueError("Location is not set.")
+
+    #     weather_url = f"https://api.open-meteo.com/v1/forecast?latitude={self.location['latitude']}&longitude={self.location['longitude']}&{category}={parameter}&forecast_days={forecast_days}"
+        
+    #     response = requests.get(weather_url, headers=self.headers, timeout=10)
+    #     data = response.json()
+    #     if self.debug_mode:
+    #         print(f"Weather data: {data}")  # Debugging line to check the weather data
+    #     response_code = response.status_code
+    #     if self.debug_mode:
+    #         print('Response code: ', response_code)
+
+    #     if category in data and parameter in data[category]:
+    #         return data[category][parameter]
+    #     else:
+    #         raise ValueError(f"{parameter} is not available in the {category} weather data.")
 
     def set_api_key(self, type, key):
         if type == "ipgeolocation":
-            self.ipgeoloation_api_key = key
+            self.ipgeolocation_api_key = key
         else:
             raise ValueError("Unsupported API type. Currently only 'ipgeolocation' is supported.")
         
@@ -183,9 +194,9 @@ class Client:
         if not self.location:
             raise ValueError("Location is not set.")
 
-        astro_url = f"https://api.ipgeolocation.io/v3/astronomy?apiKey={self.ipgeoloation_api_key}&lat={self.location['latitude']}&long={self.location['longitude']}"
+        astro_url = f"https://api.ipgeolocation.io/v3/astronomy?apiKey={self.ipgeolocation_api_key}&lat={self.location['latitude']}&long={self.location['longitude']}"
         
-        response = requests.get(astro_url)
+        response = requests.get(astro_url, headers=self.headers, timeout=10)
         data = response.json()
         if self.debug_mode:
             print(f"Astronomy data: {data}")  # Debugging line to check the astronomy data
